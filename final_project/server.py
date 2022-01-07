80ae49a203db3b3b5af46c7569a1bc26e9167c2f
RichTony <94975715+CodeFalcon363@users.noreply.github.com>
2022-01-07 21:01:50 +0100

    Add files via upload

--- final_project/server.py
+++ final_project/server.py
@@ -2,23 +2,24 @@ import json
 from machinetranslation import translator
 from flask import Flask, render_template, request
 
+
 app = Flask("Web Translator")
 
 @app.route("/englishToFrench")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
-    englishText = translator.englishToFrench(textToTranslate) # it calls the english to french translator function in translator.py
-    return "englishText"
+    frenchText = translator.englishToFrench(textToTranslate)   # calling the english to french translation function
+    return frenchText
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
-    frenchText = translator.frenchToEnglish(textToTranslate) # it calls the french to english translator function in translator.py
-    return "frenchText"
+    englishText = translator.frenchToEnglish(textToTranslate) # calling the french  to english translation function
+    return englishText
 
 @app.route("/")
 def renderIndexPage():
-    return render_template("index.html") # the code to renders the default homepage
+   return render_template("index.html") # the code to render the default dislay page        
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
